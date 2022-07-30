917c6b64fc7e097931d18380144801860b50982f
WizMe-M <timkin.moxim@mail.ru>
2022-07-30 14:16:59 +0300

    created abstract class Command.cs and derived HelpCommand.cs from it

---
+++ ConsoleCommandTool/Commands/Command.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleCommandTool.Commands;
+
+public abstract class Command
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="name">Command's name</param>
+    /// <param name="usage">Usage example</param>
+    /// <param name="description">Description of how command executes</param>
+    protected Command(string name, string usage, string description)
+    {
+        Name = name;
+        Usage = usage;
+        Description = description;
+    }
+
+    public string Name { get; }
+    public string Usage { get; }
+    public string Description { get; }
+
+    /// <summary>
+    /// Executes command with <paramref name="args"/> in <paramref name="writer"/>
+    /// </summary>
+    /// <param name="args">Command arguments</param>
+    /// <param name="writer">Writer, that will print all command output</param>
+    public abstract void Execute(string[] args, TextWriter writer);
+}
--- ConsoleCommandTool/Commands/HelpCommand.cs
+++ ConsoleCommandTool/Commands/HelpCommand.cs
@@ -3,8 +3,12 @@
 /// <summary>
 /// Represents command "h"
 /// </summary>
-public static class HelpCommand
+public class HelpCommand : Command
 {
+    public HelpCommand(string name, string usage, string description) : base(name, usage, description)
+    {
+    }
+
     /// <summary>
     /// Command, that prints all available commands to execute
     /// </summary>
@@ -12,5 +16,10 @@ public static class HelpCommand
     public static void Execute(TextWriter textWriter)
     {
         textWriter.WriteLine("Available commands: timer, printtime, help, h");
+    }
+
+    public override void Execute(string[] args, TextWriter writer)
+    {
+        writer.WriteLine("Available commands: timer, printtime, help, h");
     }
 }
