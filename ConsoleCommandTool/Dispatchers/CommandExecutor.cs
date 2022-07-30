e99c22a597981669b3673f957e6691397e74b9ae
WizMe-M <timkin.moxim@mail.ru>
2022-07-30 14:43:25 +0300

    removed static Execution code

--- ConsoleCommandTool/Dispatchers/CommandExecutor.cs
+++ ConsoleCommandTool/Dispatchers/CommandExecutor.cs
@@ -55,27 +55,6 @@ public class CommandExecutor
         _commands.FirstOrDefault(cmd => string.Equals(cmd.Name, commandName, StringComparison.OrdinalIgnoreCase));
 
     /// <summary>
-    /// Runs inputted command
-    /// <para>Also it knows about all commands, existing in app</para>>
-    /// </summary>
-    /// <param name="args">Command and it's argument</param>
-    public static void RunCommand(string[] args)
-    {
-        var writer = Console.Out;
-        var command = args[0];
-
-        if (command == "timer")
-            TimerCommand.Execute(int.Parse(args[1]), writer);
-        else if (command == "printtime")
-            PrintTimeCommand.Execute(writer);
-        else if (command == "h")
-            HelpCommand.Execute(writer);
-        else if (command == "help")
-            DetailedHelpCommand.Execute(args[1], writer);
-        else ShowUnknownCommandError(args[0], writer);
-    }
-
-    /// <summary>
     /// Prints input-error
     /// </summary>
     /// <param name="command">Input</param>
