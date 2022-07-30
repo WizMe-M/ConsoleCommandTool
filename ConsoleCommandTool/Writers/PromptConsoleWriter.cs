913b96d4b51b11e19cd64c3f72be2795151b677a
WizMe-M <timkin.moxim@mail.ru>
2022-07-30 22:26:45 +0300

    specified two text writers

---
+++ ConsoleCommandTool/Writers/PromptConsoleWriter.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace ConsoleCommandTool.Writers;
+
+public class PromptConsoleWriter : TextWriter
+{
+    public override Encoding Encoding => Console.Out.Encoding;
+    public override void WriteLine(string? value)
+    {
+        Console.Out.WriteLine($"> {value}");
+    }
+}
---
+++ ConsoleCommandTool/Writers/RedConsoleWriter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace ConsoleCommandTool.Writers;
+
+public class RedConsoleWriter : TextWriter
+{
+    public override Encoding Encoding => Console.Out.Encoding;
+    public override void Write(char value)
+    {
+        var previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Out.Write(value);
+        Console.ForegroundColor = previousColor;
+    }
+}
